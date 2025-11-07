953393a9302c27eb63484fd2e5033011c0ac7bb3
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2025-11-07 10:41:31 -0400

    Adicionado: tela de login

--- frontend/Lista Perfeita/src/routes/AppRoutes.jsx
+++ frontend/Lista Perfeita/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import MinhaLista from '../pages/MinhaLista';
+import Login from '../pages/Login';
 
 const AppRoutes = () =>{
     return(
@@ -9,6 +10,7 @@ const AppRoutes = () =>{
             <Route element={<Home />} path="/" />
             <Route element={<MinhaLista />} path="/MinhaLista"/>
             <Route element={<NotFound />} path="*" /> 
+            <Route element={<Login />} path="/login"/>
         </Routes>
     )
 }
