8a1ece9fa6371e000cfa94f804f75c2675574373
Joao Pedro Telles Paes <jptpa.snf24@uea.edu.br>
2025-11-12 17:37:49 -0400

    trabalhando no llm

--- backend/LLM/agent.py
+++ backend/LLM/agent.py
@@ -1,11 +1,13 @@
 from google.adk.agents.llm_agent import Agent
-from .custom_agent_wrapper_function import build_agent
+from LLM.custom_agent_wrapper_function import build_agent
 
-check_agent = build_agent()
+check_agent = build_agent(name="useless_agent",
+                          desc="does nothing",
+                          instr_file_path="LLM/agent_instructions/sub.txt")
 
 root_agent = build_agent(llm="ollama/gemma3:12b",
                          name="manager",
-                         desc="mudar depois",
-                         instr_file_path="agent_instructions/root.txt",
-                         tools=[],
-                         subagents=[check_agent])
+                         desc="manages interactions between agents of the sub-agent system",
+                         instr_file_path="LLM/agent_instructions/root.txt",
+                         customtools=[],
+                         subagents=[])
